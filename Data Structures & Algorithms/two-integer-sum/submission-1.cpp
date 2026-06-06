f939360ec980ca7f08d39b56fdcf686639d1f622
Marek Szulak <73235295+2526-MAREK@users.noreply.github.com>
2026-06-06 10:35:07 +0200

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    std::vector<int32_t> twoSum(std::vector<int32_t>& nums, int32_t target) {
+        
+        for (int i = 0; i < nums.size(); i++) {
+        for (int j = i + 1; j < nums.size(); j++) {
+            if (nums[i] + nums[j] == target) {
+                return {i, j}; // Immediately returns the two indices as a vector
+            }
+        }
+    }
+    return {}; // Return empty if no solution is found
+    
+        // std::unordered_set<int32_t>  result;
+
+        // for(int32_t i = 0; i < nums.size()  ; ++i)
+        // {
+        //      for(int32_t j = 0; j < nums.size() ; ++j)
+        //      {
+        //         if((nums[i] + nums[j] == target) && (i != j) )
+        //         {
+        //             result.insert(i);
+        //             result.insert(j);
+        //         }
+                    
+        //      }
+        // }
+
+        // std::vector<int32_t> resultVector(result.begin(), result.end());
+
+
+        // std::sort(resultVector.begin(), resultVector.end());
+        // //std::cout << "j: " << j << " i: " << i << std::endl;
+        // return resultVector;
+    }
+};
